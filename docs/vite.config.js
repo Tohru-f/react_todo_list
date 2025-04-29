ad8f1efb0eaf29fbd7414ee4e3357907e000c391
Tohru Furusawa <tohru.furusawa@gmail.com>
2025-04-29 09:29:26 +0900

    reflect npm run build after CSS change

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_todo_list/"
+  base: "/react_todo_list/",
+  build: {
+    outDir: 'docs'
+  }
 })
